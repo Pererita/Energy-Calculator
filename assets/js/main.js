1e84c0f33903ce0f6e464e6f804c7e49e7c50e36
Pererita <jesus2perera3@gmail.com>
2022-02-23 22:31:56 -0400

    Nuevos cambios en HTML, CSS y JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,7 @@ var counter = document.getElementById("actualEnergy");
 counter = 3;
 var round = document.getElementById("roundNumber");
 roundNumber = 1;
-/*
-var currentCardsCounter = document.getElementById("currentCards");
-currentCardsCounter = 6;
-*/
-/**
- * Energy-round functions
- */
+
 function subtract() {
   counter -= 1;
 
@@ -31,41 +25,19 @@ function add() {
 function nextRound() {
   roundNumber += 1;
   counter += 2;
-  /*currentCardsCounter += 3;*/
 
   if (counter >= 10) {
     counter = 10;
   }
-/*
-  if (roundNumber >= 10) {
-    document.body.style.backgroundColor = "#b52930ad";
-  } else {
-    document.body.style.backgroundColor = "#c07f5aad";
-  }
-  
-  if (currentCardsCounter >= 12) {
-    currentCardsCounter = 12;
-  }
-  if (currentCardsCounter <= 0) {
-    currentCardsCounter = 3;
-  }
-  */
+
   document.getElementById("roundNumber").innerHTML = roundNumber;
   document.getElementById("actualEnergy").innerHTML = counter;
-  /*document.getElementById("currentCards").innerHTML = currentCardsCounter;*/
 }
 
 function reset() {
   counter = 3;
   roundNumber = 1;
-  /*currentCardsCounter = 6;*/
 
   document.getElementById("actualEnergy").innerHTML = counter;
   document.getElementById("roundNumber").innerHTML = roundNumber;
-  /*document.getElementById("currentCards").innerHTML = currentCardsCounter;*/
-  /*
-  resetA1Parts();
-  resetA2Parts();
-  resetA3Parts(); 
- */
 }
